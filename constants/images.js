b5b4f8fdde51324aa1c0642ed14666aa52dbc297
HasinduLanka <hlrathnayaka@gmail.com>
2022-08-31 12:53:11 +0000

    feat: optimize images into WEBP

--- constants/images.js
+++ constants/images.js
@@ -1,31 +1,31 @@
 // logo
-export { default as Logo } from '../assets/images/logo.png';
-export { default as LogoDark } from '../assets/images/logo-dark.png';
+export { default as Logo } from '../assets/images/logo.webp';
+export { default as LogoDark } from '../assets/images/logo-dark.webp';
 
 // testimonial images
-export { default as Testimonial_1 } from '../assets/images/testimonial-1.jpg';
-export { default as Testimonial_2 } from '../assets/images/testimonial-2.jpg';
-export { default as Testimonial_3 } from '../assets/images/testimonial-3.jpg';
-export { default as Testimonial_4 } from '../assets/images/testimonial-4.jpg';
-export { default as Testimonial_5 } from '../assets/images/testimonial-5.jpg';
+export { default as Testimonial_1 } from '../assets/images/testimonial-1.webp';
+export { default as Testimonial_2 } from '../assets/images/testimonial-2.webp';
+export { default as Testimonial_3 } from '../assets/images/testimonial-3.webp';
+export { default as Testimonial_4 } from '../assets/images/testimonial-4.webp';
+export { default as Testimonial_5 } from '../assets/images/testimonial-5.webp';
 
-export { default as CollaborateImg } from '../assets/images/collaborate-img.png';
+export { default as CollaborateImg } from '../assets/images/collaborate-img.webp';
 
-export { default as ServicesImg } from '../assets/images/services-exp.png';
+export { default as ServicesImg } from '../assets/images/services-exp.webp';
 
-export { default as ProjectSectionBg } from '../assets/images/proj-sec-bg.png';
+export { default as ProjectSectionBg } from '../assets/images/proj-sec-bg.webp';
 
 // cover images
 export { default as Landingsm } from '../assets/images/landing-sm.webp';
 
 // shapes images
-export { default as Shape1 } from '../assets/images/shapes/shape-1.png';
-export { default as Shape2 } from '../assets/images/shapes/shape-2.png';
-export { default as Shape3 } from '../assets/images/shapes/shape-3.png';
-export { default as Shape4 } from '../assets/images/shapes/shape-4.png';
-export { default as Shape5 } from '../assets/images/shapes/shape-5.png';
-export { default as Shape6 } from '../assets/images/shapes/shape-6.png';
-export { default as Shape7 } from '../assets/images/shapes/shape-7.png';
+export { default as Shape1 } from '../assets/images/shapes/shape-1.webp';
+export { default as Shape2 } from '../assets/images/shapes/shape-2.webp';
+export { default as Shape3 } from '../assets/images/shapes/shape-3.webp';
+export { default as Shape4 } from '../assets/images/shapes/shape-4.webp';
+export { default as Shape5 } from '../assets/images/shapes/shape-5.webp';
+export { default as Shape6 } from '../assets/images/shapes/shape-6.webp';
+export { default as Shape7 } from '../assets/images/shapes/shape-7.webp';
 
 // partners images
 export { default as Partner_deinclusive } from '../assets/images/partners/partner-deinclusive.webp';
