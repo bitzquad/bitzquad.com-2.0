75cdde117d938b463b268fbc1b81cc04b1c6ce63
Bitzquad Dev <96015729+bitzquad-dev@users.noreply.github.com>
2024-06-04 21:57:43 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -35,7 +35,7 @@ export default function Home() {
                     <OurPartners />
                     <ProjectSection />
                     {/* <Testimonials /> */}
-                    <OurTeam />
+                    (/* <OurTeam /> */}
                     <Collaborate />
                 </div>
             </LayoutMain>
